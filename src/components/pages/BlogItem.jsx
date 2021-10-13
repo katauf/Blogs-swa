bdc3a8c3e6b1277f239a08367e656e70b094e2b6
OmUgan <turkelli@gmail.com>
2021-10-14 00:24:07 +0200

    .

--- src/components/pages/BlogItem.jsx
+++ src/components/pages/BlogItem.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import moment from "moment/min/moment-with-locales";
 
 const BlogItem = ({ blogPost }) => {
   return (
     <div className="mt-4 p-2">
       <h3 className="text-success">{blogPost.title}</h3>
       <p>{blogPost.blogtext}</p>
-      <p>
-        <strong>{moment(blogPost.PostDate).locale("sv").calendar()}</strong>
-      </p>
     </div>
   );
 };
