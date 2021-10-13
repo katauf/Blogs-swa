7c0ecadaa7d364a0a581b50ba5de95315429ea9b
OmUgan <turkelli@gmail.com>
2021-10-13 12:03:46 +0200

    Changes in App.js and added BlogHeader.jsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React from "react";
+import Header from "./components/BlogHeader";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
 
 export default App;
